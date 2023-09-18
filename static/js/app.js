ab4433a4b114b52e4e23ca41f7fc38990b17b6a2
cburgyan <cburgyan@gmail.com>
2023-09-17 21:38:28 -0500

    add populated dropdown menu and work on displaying bar chart

--- static/js/app.js
+++ static/js/app.js
@@ -3,11 +3,95 @@
 const url = 'https://2u-data-curriculum-team.s3.amazonaws.com/dataviz-classroom/v1.1/14-Interactive-Web-Visualizations/02-Homework/samples.json';
 const dataPromise = d3.json(url);
 
+
+
+function plotPersonsBarData(person_id, sortedOTUsBySampleValue){
+    let sample_values = sortedOTUsBySampleValue.map(otu_record => otu_record.sample_value).slice(0,10);
+    let otu_ids = sortedOTUsBySampleValue.map(otu_record => otu_record.otu_ids).slice(0,10).map(item => 'OTU '.concat(item.toString()));
+    let otu_labels = sortedOTUsBySampleValue.map(otu_record => otu_record.otu_labels).slice(0,10);
+    let trace = {
+        x: sample_values,
+        y: otu_ids,
+        text: otu_labels,
+        type: 'bar',
+        orientation: 'h'
+    };
+
+    let layout ={
+        title: `${person_id}'s Operational Taxonomic Units (OTU) Profile`,
+        margins:{
+            
+        }
+    };
+
+    Plotly.newPlot('bar', [trace], layout);
+
+}
+
+
 dataPromise.then(function(data){
     console.log(data);
     console.log(`data.names for 100th person: ${data.names[100]}`);
     console.log(`data.samples for 100th person: ${data.samples[100].otu_labels[0]}`);
     console.log(`data.metadata for 100th person: ${data.metadata[100].age}`);
+    let dropDownMenu = d3.select('#selDataset');
+    // console.log(dropDown);
+    // let selection = d3.select("body");
+  
+    // // Creating and appending
+    // // a div to the body
+    // selection.append(d3.creator("div"));
+    // let div = document.querySelector("div");
+      
+    // div.innerText = "Div tag created using d3.creator()";
+
+    // Populate the the html Select Tag for the dropdown menu with person ids
+    // and corresponding index for each person's "value" for the html option
+    // tag that is created.
+    for (let i = 0; i < data.names.length; i++){
+        let option1 = dropDownMenu.append('option').text(data.names[i]);
+        option1.attr('value', i);
+    }
+
+    function bindSampleValues_Otu_ids_Otu_labels(sample_values, otu_ids, otu_labels){
+        let listDict = [];
+        for (let i = 0; i < sample_values.length; i++){
+            let dict = {
+                'sample_value': sample_values[i],
+                'otu_id': otu_ids[i],
+                'otu_label': otu_labels[i]
+            };
+
+            listDict.append(dict);
+        }
+        return listDict;
+    }
+
+    function sortOTUPopulationsBySampleValue(sample_values, otu_ids, otu_labels){
+
+        let listOfDicts = bindSampleValues_Otu_ids_Otu_labels(sample_values, otu_ids, otu_labels);
+        return listOfDicts.sort((firstOTU, secondOTU) => secondOTU - firstOTU);
+    }
+
+    function createBarChart(){
+        let dropDownMenu1 = d3.select('#selDataset');
+        
+        // console.log(`dropDownMenu1.property('value'): ${dropDownMenu1.property('value')}`);
+
+        let person_index = dropDownMenu1.property('value');
+        console.log(`person_index: ${person_index}`);
+        let person_id = data.samples[person_index].id;
+        console.log(`person_id: ${person_id}`);
+        // console.log(`data.samples[person_index]: ${data.samples[person_index]}`);
+
+        let sample_values = data.samples[person_index].sample_values;
+        let otu_ids = data.samples[person_index].otu_ids;
+        let otu_labels = data.samples[person_index].otu_labels;
+        let sortedOTUsBySampleValue = sortOTUPopulationsBySampleValue(sample_values, otu_ids, otu_labels);
+        plotPersonsBarData(person_id, sortedOTUsBySampleValue);
+    }
+
+    dropDownMenu.on('change', createBarChart, data);
 });
 
 
