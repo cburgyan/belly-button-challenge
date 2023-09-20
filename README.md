a70aaea67bb73fd06c7be40be63db8bd3ec4d445
cburgyan <cburgyan@gmail.com>
2023-09-19 20:27:10 -0500

    add comments, clean up code, and work on README

--- README.md
+++ README.md
@@ -1,1 +1,33 @@
 # belly-button-challenge
+
+---
+ ### Karoly Burygan
+---
+## Sources:
+<ol>
+    <li>
+        "The Best Way to Deep Copy Objects or Arrays in JavaScript - structuredClone()". dcode. Sept. 7, 2022. https://www.youtube.com/watch?v=LnBxD1aXw7I&t=330s 
+        <ul>
+            <li>
+                Used in app.js here: data = structuredClone(data1);
+            </li>
+        </ul>
+   </li>
+   <li>
+        "Styling Markers in JavaScript". Plotly.com. n.d. https://plotly.com/javascript/marker-style/
+        <ul>
+            <li>
+                Used in app.js here: color: otu_ids.map(item => `rgb(${item % 256}, ${Math.floor(item / 2) % 256}, ${Math.floor(item / 3) % 256})`),
+            </li>
+        </ul>
+   </li>
+   <li>
+        "JavaScript String concat()". Refsnes Data. n.d. https://www.w3schools.com/jsref/jsref_concat_string.asp
+        <ul>
+            <li>
+                Used in app.js here: let otu_ids = sortedOTUsBySampleValue.otu_ids.slice(0,10).map(item => 'OTU '.concat(item.toString())).reverse();
+            </li>
+        </ul>
+   </li>
+
+</ol>
--- static/js/app.js
+++ static/js/app.js
@@ -1,18 +1,24 @@
 
-
+// URL to retrieve OTU data
 const url = 'https://2u-data-curriculum-team.s3.amazonaws.com/dataviz-classroom/v1.1/14-Interactive-Web-Visualizations/02-Homework/samples.json';
+
+// Retrieve 'sample.json' from 'url' using d3.json
 const dataPromise = d3.json(url);
 
+// Global variable to hold a deep copy of retrieved data
 let data = null;
 
 
+// Function to create bar chart of top 10 most common OTUs for the person with id 'person_id'
 function plotPersonsBarData(person_id, sortedOTUsBySampleValue){
+    
+    // Save top 10 most common OTU sample values, otu ids, and otu labels to 'sample_values', 'otu_ids',
+    // and 'otu_labels', respectively
     let sample_values = sortedOTUsBySampleValue.sample_values.slice(0,10).reverse();
-    console.log(`sortedOTUsBySampleValue.otu_labels[0]: ${sortedOTUsBySampleValue.otu_labels[0]}`);
     let otu_ids = sortedOTUsBySampleValue.otu_ids.slice(0,10).map(item => 'OTU '.concat(item.toString())).reverse();
     let otu_labels = sortedOTUsBySampleValue.otu_labels.slice(0,10).reverse();
 
-    let numOfSampleValues = sample_values.length;
+    // Plot sample values (x-axis) by otu ids (y-axis) in a horizontal bar chart
     let trace = {
         x: sample_values,
         y: otu_ids,
@@ -22,12 +28,19 @@ function plotPersonsBarData(person_id, sortedOTUsBySampleValue){
         width: .8
     };
 
-    console.log(`sample_values.length: ${sample_values.length}`)
-    let marginSize = 800;
-
     let layout ={
-        title: `${person_id}'s Operational Taxonomic Units (OTU) Profile`,
-        width: 800
+        title: `${person_id}'s Operational Taxonomic Units (OTUs) Profile`,
+        width: 800,
+        xaxis: {
+            title:{
+                text: 'OTU Population'
+            }
+        },
+        yaxis: {
+            title:{
+                text: 'OTU ID'
+            }
+        },
     };
 
     Plotly.newPlot('bar', [trace], layout);
@@ -35,13 +48,16 @@ function plotPersonsBarData(person_id, sortedOTUsBySampleValue){
 }
 
 
+// Function to create bubble chart of all OTUs for the person with id 'person_id'
 function plotPersonsBubbleChart(person_id, sortedOTUsBySampleValue){
+    
+    // Save all OTU sample values, otu ids, and otu labels to 'sample_values', 'otu_ids',
+    // and 'otu_labels', respectively
     let sample_values = sortedOTUsBySampleValue.sample_values;
-    console.log(`sortedOTUsBySampleValue.otu_labels[0]qq: ${sortedOTUsBySampleValue.otu_labels[0]}`);
     let otu_ids = sortedOTUsBySampleValue.otu_ids;
     let otu_labels = sortedOTUsBySampleValue.otu_labels;
 
-    let numOfSampleValues = sample_values.length;
+    // Plot sample values (x-axis) by otu ids (y-axis) in a horizontal bar chart
     let trace = {
         x: otu_ids,
         y: sample_values,
@@ -56,17 +72,18 @@ function plotPersonsBubbleChart(person_id, sortedOTUsBySampleValue){
         }
     };
 
-    console.log(`sample_values.length: ${sample_values.length}`)
-    let marginSize = 800;
-
     let layout ={
-        title: `${person_id}'s Operational Taxonomic Units (OTU) Profile`,
+        title: `${person_id}'s Operational Taxonomic Units (OTUs) Profile`,
         xaxis: {
             title:{
-                text: 'OTU'
+                text: 'OTU ID'
             }
         },
-        // width: 800
+        yaxis: {
+            title:{
+                text: 'OTU Population'
+            }
+        },
     };
 
     Plotly.newPlot('bubble', [trace], layout);
@@ -74,6 +91,52 @@ function plotPersonsBubbleChart(person_id, sortedOTUsBySampleValue){
 }
 
 
+
+function plotPersonsGaugeWashFrequency(person_index1){
+    let washFrequency = data.metadata[person_index1].wfreq;
+    let colorStep = 25;
+    let trace = 
+    {
+        domain: { x: [0, 1], y: [0, 1] },
+        value: washFrequency,
+        title: { text: "Scrubs per Week" },
+        type: "indicator",
+        mode: "gauge",
+        guage: {
+            axis: { range: [null, 9]},
+            //     ticks: 'inside', 
+            //     showticklabels: false },
+            // bar: { color: "darkblue" },
+            // bgcolor: "brown",
+            // borderwidth: 2,
+            // bordercolor: "gray",
+            steps: [
+                { range: [0, 1], color: `rgb(${colorStep * 1}, 255, ${colorStep * 1})`},
+                { range: [1, 2], color: `rgb(${colorStep * 2}, 255, ${colorStep * 2})`},
+                { range: [2, 3], color: `rgb(${colorStep * 3}, 255, ${colorStep * 3})`},
+                { range: [3, 4], color: `rgb(${colorStep * 4}, 255, ${colorStep * 4})`},
+                { range: [4, 5], color: `rgb(${colorStep * 5}, 255, ${colorStep * 5})`},
+                { range: [5, 6], color: `rgb(${colorStep * 6}, 255, ${colorStep * 6})`},
+                { range: [6, 7], color: `rgb(${colorStep * 7}, 255, ${colorStep * 7})`},
+                { range: [7, 8], color: `rgb(${colorStep * 8}, 255, ${colorStep * 8})`},
+                { range: [8, 9], color: `rgb(${colorStep * 9}, 255, ${colorStep * 9})` }
+              ]
+        }
+    };
+
+    let data1 = [trace];
+
+    let layout = {};// width: 600, height: 450, margin: { t: 100, b: 100 } };
+
+    Plotly.newPlot('gauge', data1, layout);
+
+}
+
+
+// Function to pack up each OTU's id (an element of 'otu_ids'), sample value (an element of 
+// 'sample_values') and label (an element of 'otu_labels') so that their correspondence to 
+// each other is maintained in a dictionary structure and not just by their commonly shared
+// index (because the index of the sample value may change when it is sorted).
 function bindSampleValues_Otu_ids_Otu_labels(sample_values, otu_ids, otu_labels){
     let listDict = [];
     for (let i = 0; i < sample_values.length; i++){
@@ -89,6 +152,9 @@ function bindSampleValues_Otu_ids_Otu_labels(sample_values, otu_ids, otu_labels)
 }
 
 
+// Function to reestablish array structures for the sample values (into 'sample_values'), 
+// the uto ids (into 'uto_ids'), and the uto labels (into 'uto_labels') for the purpose of
+// using those arrays as the x values, y values, and text values in the bar chart to come.
 function unbindUTORecords(sortedListOfDicts){
     let sample_values = [];
     let otu_ids = [];
@@ -104,26 +170,24 @@ function unbindUTORecords(sortedListOfDicts){
 }
 
 
+// Function to sort OTUs by sample value of the OTU
 function sortOTUPopulationsBySampleValue(sample_values, otu_ids, otu_labels){
-    // In order to keep the otu_id, its (the otu's) corresponding otu_label, and (the otu's) its corresponding 
+    // In order to keep the otu_id, its (otu's) corresponding otu_label, and its (otu's) corresponding 
     // sample_value together during the sorting of the sample_values, they have to be bound together before the
-    // the sort.
+    // the sort-->bindSampleValues_Otu_ids_Otu_labels binds them together.
     let listOfDicts = bindSampleValues_Otu_ids_Otu_labels(sample_values, otu_ids, otu_labels);
 
     // Sorting the bounded otu records by sample_value size.
     let sortedListOfDicts = listOfDicts.sort((firstOTU, secondOTU) => secondOTU.sample_value - firstOTU.sample_value);
     
-    // Unbinding the otu records.
+    // Unbinding the otu records-->unbindUTORecords unbinds UTO records in preparation for plotting the sorted data.
     let unboundUTORecords = unbindUTORecords(sortedListOfDicts);
 
-    // // Quick sanity check printout of results.
-    // console.log(`unboundUTORecords.sample_values[0]: ${unboundUTORecords.sample_values[0]}`);
-    // console.log(`unboundUTORecords.otu_ids[0]: ${unboundUTORecords.otu_ids[0]}`);
-    // console.log(`unboundUTORecords.otu_labels[0]: ${unboundUTORecords.otu_labels[0]}`);
     return unboundUTORecords;
 }
 
 
+// Function to update the 'Demographic Info' when a new person is selected from the dropdown menu
 function loadPersonsDemographicData(person_index1){
     // Grab each demographic field by its corresponding html tag id and update its 'text'
     d3.select('#demoId').text(`id: ${data.names[person_index1]}`);
@@ -136,41 +200,42 @@ function loadPersonsDemographicData(person_index1){
 }
 
 
+// Fucntion to be called every time a new id is selected from the dropdown menu. When this funciton
+// is called it executes the process that displays the data for the new person selected.
 function optionChanged(person_index){
     if (data != null){
-        loadPersonsDemographicData(person_index);
-        // console.log('hi');
-        // console.log(`data1.names for 100th person: ${data.names[100]}`);
-        // console.log('----------');
+        // Initialize variables to pass to the following functions
         let person_id = data.samples[person_index].id;
-        // console.log(`person_id: ${person_id}`);
-        // console.log(`data1.samples[person_index]: ${data1.samples[person_index]}`);
-
         let sample_values = data.samples[person_index].sample_values;
         let otu_ids = data.samples[person_index].otu_ids;
         let otu_labels = data.samples[person_index].otu_labels;
+        
+        // Load 'Demographic Info'
+        loadPersonsDemographicData(person_index);
         let sortedOTUsBySampleValue = sortOTUPopulationsBySampleValue(sample_values, otu_ids, otu_labels);
         plotPersonsBarData(person_id, sortedOTUsBySampleValue);
         console.log('hi3');
         plotPersonsBubbleChart(person_id, sortedOTUsBySampleValue);
+        plotPersonsGaugeWashFrequency(person_index);
     }
 }
 
 
+// Store the OTU data and initialize the .html file with new tags and values from the data.
 dataPromise.then(function(data1){
 
     // Make a deep copy of the data ('data1') retrieved in the PromiseResults of 
     // .json(url) result ('dataPromise').
     data = structuredClone(data1);
-    
+
     // // Quick printout-sanity-check
-    // console.log(data1);
+    console.log(data1);
     // console.log(`data.names for 100th person: ${data1.names[100]}`);
     // console.log(`data.samples for 100th person: ${data1.samples[100].otu_labels[0]}`);
     // console.log(`data.metadata for 100th person: ${data1.metadata[100].age}`);
 
 
-    function initialize(data){
+    function initialize(){
     
         // Populate the the html Select Tag for the dropdown menu with person_ids as
         // the 'text' of the html-option-tag and corresponding index for the person's
@@ -181,7 +246,12 @@ dataPromise.then(function(data1){
             option1.attr('value', i);
         }
 
-        // Populate 'Demographic Info'
+        // Populate 'Demographic Info' for first time including creation and insertion of
+        // the html tags that will contain the populating info. And give the newly inserted
+        // html tags unique ids to be referred to later in when the 'Demographic Info' needs
+        // to be updated (the 'Demographic Info' is simple the initial person in the data
+        // that corresponds to person_index zero of the dropdown menu (which is currently 
+        // person_id equal to '940')).
         let demographicDiv = d3.select('#sample-metadata');
         let idDemo = demographicDiv.append('p').text(`id: ${data1.names[0]}`);
         idDemo.attr('id','demoId');
@@ -200,20 +270,23 @@ dataPromise.then(function(data1){
 
 
 
-        // Create initial bar chart and bubble chart
+        // Create initial bar chart and bubble chart for the initially listed person id in the
+        // dropdown menu corresponding to person_index equal to '0'.
         let sample_values = data.samples[0].sample_values;
         let otu_ids = data.samples[0].otu_ids;
         let otu_labels = data.samples[0].otu_labels;
 
-
+        // Sort the sample values for person corresponding to person_index equal to '0'
         let sortedOTUsBySampleValue = sortOTUPopulationsBySampleValue(sample_values, otu_ids, otu_labels);
+
+        // Plot the person_index-0's bar chart and bubble chart.
         plotPersonsBarData(data.samples[0].id, sortedOTUsBySampleValue);
         plotPersonsBubbleChart(data.samples[0].id, sortedOTUsBySampleValue);
-
+        plotPersonsGaugeWashFrequency(0)
 
     }
 
-    initialize(data1);
+    initialize();
 });
 
 
